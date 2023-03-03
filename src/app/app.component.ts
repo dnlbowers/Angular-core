88cad1b58a666d75240a262ba649cad126ea311d
David Bowers <dbowers@greentube.com>
2023-03-03 16:51:30 +0100

    finish section3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import {COURSES} from '../db-data';
+import { CourseCardComponent } from './course-card/course-card.component';
 import { Course } from './model/course';
 
 @Component({
@@ -7,12 +8,48 @@ import { Course } from './model/course';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
 
   courses = COURSES;
 
-  onCourseSelected(course: Course) {
-    console.log("App Component - click event bubbled.....", course);
+  @ViewChildren(CourseCardComponent, {read: ElementRef})
+  cards: QueryList<ElementRef>;
 
+  // @ViewChild('cardRef1', {read: ElementRef})
+  // card1: ElementRef;
+
+  // @ViewChild('courseImg')
+  // courseImg: ElementRef;
+
+  constructor() {
+
+  }
+
+  ngAfterViewInit() {
+    // this.cards.changes.subscribe(
+    //   cards => console.log(cards)
+    // )
+    // // console.log("courseImg", this.courseImg);
+    // console.log(this.cards.changes);
+    console.log(this.cards);
+  }
+
+  onCourseSelected(course: Course) {
+    // console.log("containerDiv", this.card1);
+
+  }
+
+  onCoursesEdited() {
+    this.courses.push(
+      {
+        id: 1,
+        description: "Angular Core Deep Dive",
+        iconUrl: 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
+        longDescription: "A detailed walk-through of the most important part of Angular - the Core and Common modules",
+        category: 'INTERMEDIATE',
+        lessonsCount: 10
+      },
+      
+    )
   }
 }
